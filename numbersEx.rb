c1771a5b2bea2e6af42e5ff4bb8c7706f7d64970
ohdDev <w.ohd95@gmail.com>
2022-01-16 19:27:58 +0300

    third comm

--- numbersEx.rb
+++ numbersEx.rb
@@ -17,14 +17,14 @@ puts x - 2
 puts x/2
 
 #E. Add 1/1/4, print the result
-puts x + 1/1/4
+puts x.to_r + Rational('1/4')
 
 # F. print the numerator and denominator of result
-puts Rational(x).numerator
-puts Rational(x).denominator
+puts x.numerator
+puts x.denominator
 
 # G. Multiply by ⅓ , print the result
-puts x * (1/3)
+puts x * Rational('1/3')
 
 # H. Round to 3 decimal places, print the result
 puts x.round(3)
