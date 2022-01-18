07f24fdb97930f71e547560263761ed79c503932
ohdDev <w.ohd95@gmail.com>
2022-01-18 08:54:55 +0300

    loop commit

--- loopEx.rb
+++ loopEx.rb
@@ -1,59 +1,59 @@
 #A.
 arr=[1,2,3,4,5,6,7,8,9,10]
-puts arr
+p arr
 
 #B.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr 
-  if elm == 5 || elm == 6
+for el in arr 
+  if el == 5 || el == 6
     next
   end
-  puts elm
+  puts el
 end
 
 #C.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr 
-  if elm < 6
-    puts elm
+for el in arr 
+  if el < 6
+    puts el
   end
 end
 
 #D.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr
-   if elm.even?
-      puts elm
+for el in arr
+   if el.even?
+      puts el
    end
 end
 
 #E.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr
-   if elm.odd?
-      puts elm
+for el in arr
+   if el.odd?
+      puts el
    end
 end
 
 #F.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr
-  if(elm == 1)
+for el in arr
+  if(el == 1)
     next
   end
-  if(elm <= 9)
-    puts elm
-  elsif(elm == 1)
+  if(el <= 9)
+    puts el
+  elsif(el == 1)
     next
   end
 end
 
 #G.
 arr=[1,2,3,4,5,6,7,8,9,10]
-for elm in arr
-  if elm.odd?
-    puts "#{elm} : odd"
-  elsif elm.even?
-    puts "#{elm} : even"
+for el in arr
+  if el.odd?
+    puts "#{el} : odd"
+  elsif el.even?
+    puts "#{el} : even"
   end
 end
